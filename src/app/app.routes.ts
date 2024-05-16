8fb761eb89bfd5ad388d179e0ce509710598f14e
Tuan17tuiii <tn0186784@gmail.com>
2024-05-16 13:27:43 +0700

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { PostUpComponent } from './postup.component';
+import { AboutComponent } from './about.component';
+import { ContacComponent } from './contact.component';
+import { Property_listComponent } from './property_list.component';
+import { Property_typeComponent } from './property_type.component';
+import { Property_agentComponent } from './property_agent.component';
 
 export const routes: Routes = [
    {
@@ -11,6 +16,26 @@ export const routes: Routes = [
    {
       path : 'post-up',
       component : PostUpComponent
+     },
+     {
+      path:"about",
+      component:AboutComponent
+     },
+     {
+      path:"contact",
+      component:ContacComponent
+     },
+     {
+      path:"property_list",
+      component:Property_listComponent
+     },
+     {
+      path:"property_type",
+      component:Property_typeComponent
+     },
+     {
+      path:"property_agent",
+      component:Property_agentComponent
      }
 ];
 
