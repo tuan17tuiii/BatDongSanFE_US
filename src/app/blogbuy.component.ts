8ec9c003061ed30cc8c8bee23d69267024a7232c
Tuan17tuiii <tn0186784@gmail.com>
2024-05-23 09:50:45 +0700

    first commit

--- src/app/blogbuy.component.ts
+++ src/app/blogbuy.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { RealStateAPIService } from './services/realstate.service';
+import { RealState } from './entities/realstate.entities';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterLink],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: 'blogbuy.component.html',
-  
-})
-export class BlogbuyComponent implements OnInit{
-  ngOnInit(): void {
 
+})
+export class BlogbuyComponent implements OnInit {
+  realstates:RealState[]
+  constructor(
+    private realstatesv: RealStateAPIService
+
+  ) {
+
+  }
+  ngOnInit(): void {
+    this.realstatesv.findAll().then(
+      res => { this.realstates=res as RealState[],
+        console.log(this.realstates)
+       }
+      ,
+      err => {
+        console.log("kloi r ba oi")
+      }
+    )
   }
 }
