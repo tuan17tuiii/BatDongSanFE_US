28f0d731e8167224924ce63d0f8fc8edfbed83a3
quochuyvo13 <quochuyvo13@gmail.com>
2024-05-23 07:55:44 +0700

    commit

--- src/app/blogsellerdetails.component.ts
+++ src/app/blogsellerdetails.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
+import { RealStateAPIService } from './services/realstate.service';
+import { ImageRealStateAPIService } from './services/image.service';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +12,15 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   
 })
 export class BlogsellerdetailsComponent implements OnInit{
+  constructor(
+    private imageService : ImageRealStateAPIService ,
+    private realstateService : RealStateAPIService , 
+    private activatedRoute : ActivatedRoute
+) { }
   ngOnInit(): void {
-
+    this.activatedRoute.paramMap.subscribe(p=>{
+      let id = p.get('id'); 
+      console.log(id);
+    })
   }
 }
