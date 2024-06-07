dda942d30c051c661975493f7937225fa0334bd4
Tuan17tuiii <tn0186784@gmail.com>
2024-06-07 22:55:29 +0700

    inforfomr

--- src/app/blognew.component.ts
+++ src/app/blognew.component.ts
@@ -13,7 +13,9 @@ import { ImageRealStateAPIService } from './services/image.service';
   
 })
 export class BlognewComponent implements OnInit{
+index:number=6
 news:News[]
+newsfull:News[]
 newsst1:News
 imgsurl:string
 id:string
@@ -33,9 +35,10 @@ id:string
 }
 async Renderr() :Promise<void>{
   try {
-    this.news = await this.newsSv.FindAllNews() as News[];
+    this.newsfull = await this.newsSv.FindAllNews() as News[];
+    this.news =  this.newsfull.slice(0, 7);
     this.newsst1 = this.news[0];
-
+console.log(this.news)
     this.activatedRoute.paramMap.subscribe(async p => {
       this.id = p.get('id');
       let newsId = Number(this.id);
@@ -43,6 +46,7 @@ async Renderr() :Promise<void>{
       if (newsId > 0) {
         try {
           this.newsst1 = await this.newsSv.FindbynewsId(this.id) as News;
+         
         } catch (error) {
           console.error('Error fetching news by ID:', error);
           // Handle error (e.g., show a message to the user)
@@ -59,6 +63,29 @@ async Renderr() :Promise<void>{
 update(id:string){
   if(this.news.find(x=>Number(x.id)==Number(id))!=null){
     this.newsst1=this.news.find(x=>Number(x.id)==Number(id))
+
+    const index = this.news.findIndex(x => Number(x.id) === Number(id));
+    
+    if (index !== -1) {
+      // Xóa phần tử khỏi mảng news
+      this.news.splice(index, 1);
+  
+      // Lấy phần tử mới từ mảng finall (ví dụ: lấy phần tử đầu tiên)
+      this.index=this.index+1
+      if(this.index>this.newsfull.length-1){
+        this.index=0
+      }
+   
+      const newElement = this.newsfull[this.index];
+     
+      // Thêm phần tử mới vào mảng news
+      this.news.push(newElement);
+  
+      // Cuộn lên đầu trang
+      window.scrollTo(0, 0);
+  
+      console.log('Updated news:', this.news);
+    }
   }
 }
 }
